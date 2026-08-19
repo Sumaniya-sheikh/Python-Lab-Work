cfef9cd330826e891ba6d8416dc3cd89a6dd336e
Sumaniya Sheikh <sumaniyasheikh1234@gmail.com>
2026-08-19 19:50:27 +0530

    Update Q3.py

--- week2/Q3.py
+++ week2/Q3.py
@@ -1,7 +1,12 @@
-nums= int(input("enter a range for prime number = "))
-count=0
-for i in range(1, nums+1):
-    if num%i==0:
-        count+1
-        
-    
+start=int(input("enter the starting number = "))
+end=int(input("enter the end range = "))
+for num in range(start, end+1):
+    if num<2:
+        continue
+    is_prime =True
+    for i in range(2, int(num ** 0.5)+1):
+        if num%i==0:
+            is_prime=False
+            break
+    if is_prime:
+        print(num)
