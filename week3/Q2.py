ed7daee4b018a7415fd79e9241ff3608c4960650
Sumaniya Sheikh <sumaniyasheikh1234@gmail.com>
2026-08-25 19:48:42 +0530

    Add files via upload

---
+++ week3/Q2.py
@@ -0,0 +1,20 @@
+# 2. Write a program to print the following star pattern using the for loop:
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+for i in range(1, 10):
+        print()
+        if i<=5:
+             for j in range(1, i+1):
+                                     print("*", end=" ")
+        else:         
+           for j in range(10-i):
+                                  print("*", end=" ")
+   
